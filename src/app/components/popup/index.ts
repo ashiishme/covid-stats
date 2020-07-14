c3c70a92bdbdbb2674af6ec551fea3bf46f92d99
ashiishme <iloveh4ck1ng@gmail.com>
2020-07-14 18:46:25 +0545

    refactor(comp): remove settings option

--- src/app/components/popup/index.ts
+++ src/app/components/popup/index.ts
@@ -1,2 +1,1 @@
 export { default as Dashboard } from './dashboard/dashboard';
-export { default as Settings } from './settings/settings';
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from 'react';
 
-import { Dashboard, Settings } from './components/popup';
+import { Dashboard } from './components/popup';
 import { Container } from 'styles';
 const App: FC = () => {
     return (
         <Container className="ashiishme_covid_container">
             <Dashboard />
-            <Settings />
         </Container>
     );
 };
